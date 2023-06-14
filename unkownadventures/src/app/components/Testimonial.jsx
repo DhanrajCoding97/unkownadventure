71249c0d61da010729f847eb78c5697480047212
DhanrajCoding97 <dhanrajcoding@gmail.com>
2023-06-14 18:35:54 +0530

    fixed testimonial padding

--- unkownadventures/src/app/components/Testimonial.jsx
+++ unkownadventures/src/app/components/Testimonial.jsx
@@ -25,11 +25,11 @@ const Testimonial = () => {
         }}
         
         breakpoints={{
-          "@0.00": {
+          0: {
             slidesPerView: 1,
             spaceBetween: 10,
           },
-          "@1.50": {
+          1280: {
             slidesPerView: 2,
             spaceBetween: 20,
           },
@@ -41,8 +41,8 @@ const Testimonial = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <div className="cursor-grab px-10 py-12 bg-white rounded-xl text-black text-left relative">
-            <p>
+          <div className="cursor-grab p-4 xl:px-10 xl:py-12 bg-white rounded-xl text-black text-left relative">
+            <p className=" text-md font-light">
             Unknown Adventures organized pre masoon breakfast ride with royal enfield , We wnt place called wada which had amazing scenic views, this was my first long ride and enjoyed every single minute of this ride,the ride was very well manged at every pit stop they were provided us water and some snacks to keep us energetic, I definitely look forward go on for more such amazing rides with unknown Adventures.
             </p>
             <span className="pt-4 flex justify-center">
@@ -57,7 +57,7 @@ const Testimonial = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="cursor-grab px-10 py-12 bg-white rounded-xl text-black text-left relative">
+        <div className="cursor-grab p-4 xl:px-10 xl:py-12 bg-white rounded-xl text-black text-left relative">
             <p>
             They had organised a breakfast ride for Royal Enfield riders to a place called Pali. This was my first ever ride and excursion with them and loved every part of it. Not even for a moment could I feel that I was new there, they made sure that the entire experience should be remembered forever. I have made new connections, new memories with the entire team of Unknown Adventures and I look forward to many such adventures with them. I highly recommend them to everyone, be it for family, friends, small groups, big groups, rides, picnics anything
             </p>
@@ -73,7 +73,7 @@ const Testimonial = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="cursor-grab px-10 py-12 bg-white rounded-xl text-black text-left relative">
+        <div className="cursor-grab p-4 xl:px-10 xl:py-12 bg-white rounded-xl text-black text-left relative">
             <p>
             Nicely executed the ktm urban escape ride. They took us to adventure trials where we did the kayaking. All of them were extremely cooperative and managed the ride very well. Overall a very nice experience
             </p>
@@ -89,7 +89,7 @@ const Testimonial = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="cursor-grab px-10 py-12 bg-white rounded-xl text-black text-left relative">
+        <div className="cursor-grab p-4 xl:px-10 xl:py-12 bg-white rounded-xl text-black text-left relative">
             <p>
             Unknown Adventures organized pre masoon breakfast ride with royal enfield , We wnt place called wada which had amazing scenic views, this was my first long ride and enjoyed every single minute of this ride,the ride was very well manged at every pit stop they were provided us water and some snacks to keep us energetic, I definitely look forward go on for more such amazing rides with unknown Adventures.
             </p>
@@ -105,7 +105,7 @@ const Testimonial = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="cursor-grab px-10 py-12 bg-white rounded-xl text-black text-left relative">
+          <div className="cursor-grab p-4 xl:px-10 xl:py-12 bg-white rounded-xl text-black text-left relative">
             <p>
             Unknown Adventures organized pre masoon breakfast ride with royal enfield , We wnt place called wada which had amazing scenic views, this was my first long ride and enjoyed every single minute of this ride,the ride was very well manged at every pit stop they were provided us water and some snacks to keep us energetic, I definitely look forward go on for more such amazing rides with unknown Adventures.
             </p>
@@ -121,7 +121,7 @@ const Testimonial = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="cursor-grab px-10 py-12 bg-white rounded-xl text-black text-left relative">
+          <div className="cursor-grab p-4 xl:px-10 xl:py-12 bg-white rounded-xl text-black text-left relative">
             <p>
             Unknown Adventures organized pre masoon breakfast ride with royal enfield , We wnt place called wada which had amazing scenic views, this was my first long ride and enjoyed every single minute of this ride,the ride was very well manged at every pit stop they were provided us water and some snacks to keep us energetic, I definitely look forward go on for more such amazing rides with unknown Adventures.
             </p>
